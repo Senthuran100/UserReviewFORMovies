f4467792fa0ef14875bf49c14c2b51b8e7d562c9
Senthuran100 <32807418+Senthuran100@users.noreply.github.com>
2018-03-13 11:52:25 +0530

    Update MoviesUserReview.py

--- MoviesUserReview.py
+++ MoviesUserReview.py
@@ -5,8 +5,8 @@ import openpyxl
 import pandas as pd
 review=[]
 movie=[]
-path='H:\IFS\IMDB\\test.xlsx'
-path1='H:\IFS\IMDB\\test1.xlsx'
+path='H:\IFS\IMDB\\test.xlsx' # Excel sheet containing the name of the movies 
+path1='H:\IFS\IMDB\\test1.xlsx' # Excel sheet containing the result from the IMDB which contain the user review for each movie
 df = pd.read_excel(path, sheetname='Sheet1')
 for row in df['Movies']:
     try:
